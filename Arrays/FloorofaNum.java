9a84a2cfff4a6ef0c577eb72a4157792399ccdd1
Abhishek <abhipro01@gmail.com>
2024-12-30 22:58:41 +0530

    Floor of num binary search application

---
+++ Arrays/FloorofaNum.java
@@ -0,0 +1,22 @@
+public class FloorofaNum {
+    static int bFloor(int[] arr,int tar){
+        int start = 0;
+        int end = arr.length -1;
+        while (start <= end) {
+            int mid = start + end - start / 2;
+            if(tar > mid){
+                start = mid + 1;
+            }else if (tar < mid)
+            {
+                end = mid - 1;
+            }
+        }
+        return end;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12,14,23,35,42,56,68,73,82,88,95};
+        int target = 20;
+        System.out.println("Floor value of "+target+ " value  in array : "+bFloor(arr,target) );
+    }
+}
