b5a3de26fc3ccc678958495a5b254b5adbc9c80b
Abhishek <abhipro01@gmail.com>
2024-12-31 07:05:36 +0530

    Floor ceil solved

--- Arrays/CeilofaNum.java
+++ Arrays/CeilofaNum.java
@@ -4,19 +4,19 @@ public class CeilofaNum {
         int end = arr.length -1;
         while (start <= end) {
             int mid = start + end - start / 2;
-            if(tar > mid){
+            if(tar > arr[mid]){
                 start = mid + 1;
-            }else if (tar < mid)
+            }else
             {
                 end = mid - 1;
             }
         }
-        return end;
+        return arr[start];
     }
 
     public static void main(String[] args) {
         int[] arr = {12,14,23,35,42,56,68,73,82,88,95};
-        int target = 20;
+        int target = 89;
         System.out.println("Floor value of "+target+ " value  in array : "+bCeil(arr,target) );
     }
 }
--- Arrays/FloorofaNum.java
+++ Arrays/FloorofaNum.java
@@ -4,9 +4,9 @@ public class FloorofaNum {
         int end = arr.length -1;
         while (start <= end) {
             int mid = start + end - start / 2;
-            if(tar > mid){
+            if(tar > arr[mid]){
                 start = mid + 1;
-            }else if (tar < mid)
+            }else
             {
                 end = mid - 1;
             }
@@ -16,7 +16,7 @@ public class FloorofaNum {
 
     public static void main(String[] args) {
         int[] arr = {12,14,23,35,42,56,68,73,82,88,95};
-        int target = 20;
+        int target = 93;
         System.out.println("Floor value of "+target+ " value  in array : "+bFloor(arr,target) );
     }
 }
