59c015370b64b6cd3bf0f8e700587bf89e3e1587
Abhishek <abhipro01@gmail.com>
2025-02-09 22:05:16 +0530

    Anagram added

---
+++ Basics/Anagram.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class Anagram {
+
+
+    
+    static boolean areAnagrams(String s1, String s2) {
+        
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        
+        for (char ch : s1.toCharArray()) 
+            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+  
+        for (char ch : s2.toCharArray()) 
+            charCount.put(ch, charCount.getOrDefault(ch, 0) - 1);
+  
+        for (var pair : charCount.entrySet()) {
+            if (pair.getValue() != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "nichole";
+        String s2 = "cholien";
+        System.out.println(areAnagrams(s1, s2) ? "true" : "false");
+    }
+
+
+
+}
